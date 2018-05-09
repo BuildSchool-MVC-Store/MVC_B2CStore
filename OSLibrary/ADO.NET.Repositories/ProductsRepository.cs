86b35f12cdee87f0c029778a505ad189b5e02c0f
Yi An Lin <lois.kiro@gmail.com>
2018-05-09 22:43:38 +0800

    新增ProductsRepository裡的GetAll()

--- OSLibrary/ADO.NET.Repositories/ProductsRepository.cs
+++ OSLibrary/ADO.NET.Repositories/ProductsRepository.cs
@@ -14,17 +14,96 @@ namespace OSLibrary.ADO.NET.Repositories
         {
             SqlConnection connection = new SqlConnection(
                "Server=140.126.146.49,7988;Database=2018Build;User Id=Build;Password = 123456789;"
-                );  
-            var sql=("INSERT ")
+                );
+            var sql = ("INSERT INTO Porducts VALUES (@Product_ID,@Product_Name,@UnitPrice,@Product_Types_Name,@Gender)");
+            SqlCommand command = new SqlCommand(sql, connection);
+            command.Parameters.AddWithValue("@Product_ID", model.Product_ID);
+            command.Parameters.AddWithValue("@Product_Name", model.Product_ID);
+            command.Parameters.AddWithValue("@UnitPrice", model.Product_ID);
+            command.Parameters.AddWithValue("@Product_Types_Name", model.Product_ID);
+            command.Parameters.AddWithValue("@Gender", model.Product_ID);
+
+            connection.Open();
+            command.ExecuteNonQuery();
+            connection.Close();
+        }
+        public void Update(Products model)
+        {
+            SqlConnection connection = new SqlConnection(
+             "Server=140.126.146.49,7988;Database=2018Build;User Id=Build;Password = 123456789;"
+              );
+            var sql = ("UPDATE Porducts SET Product_ID=@Product_ID,Product_Name=@Product_Name,UnitPrice=@UnitPrice,Product_Types_Name=@Product_Types_Name,Gender=@Gender");
+            SqlCommand command = new SqlCommand(sql, connection);
+            command.Parameters.AddWithValue("@Product_ID", model.Product_ID);
+            command.Parameters.AddWithValue("@Product_Name", model.Product_Name);
+            command.Parameters.AddWithValue("@UnitPrice", model.UnitPrice);
+            command.Parameters.AddWithValue("@Product_Types_Name", model.Product_Types_Name);
+            command.Parameters.AddWithValue("@Gender", model.Gender);
+
+            connection.Open();
+            command.ExecuteNonQuery();
+            connection.Close();
+        }
+        public void Delete(Products model)
+        {
+            SqlConnection connection = new SqlConnection(
+                     "Server=140.126.146.49,7988;Database=2018Build;User Id=Build;Password = 123456789;"
+                );
+            var sql = ("DELETE FROM Products WHERE Product_ID=@Product_ID");
+            SqlCommand command = new SqlCommand(sql, connection);
+            command.Parameters.AddWithValue("@Product_ID", model.Product_ID);
+
+            connection.Open();
+            command.ExecuteNonQuery();
+            connection.Close();
+        }
 
 
+
+        public Products GetByProduct_ID() {
+            SqlConnection connection = new SqlConnection(
+                     "Server=140.126.146.49,7988;Database=2018Build;User Id=Build;Password = 123456789;"
+                );
+            var sql = "SELECT * FROM Product WHERE Product_ID=@Product_ID";
+            SqlCommand command = new SqlCommand(sql,connection);
+            connection.Open();
+            var reader = command.ExecuteReader(CommandBehavior.CloseConnection);
+            var product = new Products();
+            while (reader.Read()) {
+              
+                product.Product_ID =int.Parse( reader.GetValue(reader.GetOrdinal("Product_ID")).ToString());
+                product.Product_Name = (reader.GetValue(reader.GetOrdinal("Product_Name")).ToString());
+                product.UnitPrice= int.Parse(reader.GetValue(reader.GetOrdinal("UnitPrice")).ToString());
+                product.Product_Types_Name = reader.GetValue(reader.GetOrdinal("Product_Types_Name")).ToString();
+                product.Gender =reader.GetValue(reader.GetOrdinal("Gender")).ToString();
+                
+            }
+            reader.Close();
+            return product;
 
         }
-        public void Update()
-        { }
-        public void Delete() { }
+        public IEnumerable<Products> GetAll() {
+            SqlConnection connection = new SqlConnection(
+               "Server=140.126.146.49,7988;Database=2018Build;User Id=Build;Password = 123456789;"
+                );
+            var sql = "SELECT * FROM Product WHERE Product_ID=@Product_ID";
+            SqlCommand command = new SqlCommand(sql,connection);
+            connection.Open();
+            var reader = command.ExecuteReader(CommandBehavior.CloseConnection);
+            var products = new List<Products>();
+            while (reader.Read())
+            {
+                var product = new Products();
+                product.Product_ID = int.Parse(reader.GetValue(reader.GetOrdinal("Product_ID")).ToString());
+                product.Product_Name = (reader.GetValue(reader.GetOrdinal("Product_Name")).ToString());
+                product.UnitPrice = int.Parse(reader.GetValue(reader.GetOrdinal("UnitPrice")).ToString());
+                product.Product_Types_Name = reader.GetValue(reader.GetOrdinal("Product_Types_Name")).ToString();
+                product.Gender = reader.GetValue(reader.GetOrdinal("Gender")).ToString();
+                products.Add(product);
+            }
+            reader.Close();
+            return products;
 
-        public void GetById() { }
-        public void GetAll() { }
+        }
     }
 }
