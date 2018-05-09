2ab0b384aae2c56b78233e60329d162896d1063c
User <User@DESKTOP-2LALK7K>
2018-05-09 22:11:24 +0800

    Product_Size_Quantity

---
+++ OSLibrary/ADO.NET.Repositories/ProductSizeQuantityRepository.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OSLibrary.ADO.NET.Repositories
+{
+    public class ProductSizeQuantityRepository
+    {
+        public void Create(Product_Size_Quantity model)
+        {
+            SqlConnection connection = new SqlConnection(
+                "Server=140.126.146.49,7988;Database=2018Build;User Id=Build;Password = 123456789;"
+            );
+            var sql = "INSERT INTO Product_Size_Quantity VALUES (@Product_ID,@Quantity,@Product_Size)";
+            SqlCommand command = new SqlCommand(sql, connection);
+            command.Parameters.AddWithValue("@Product_ID", model.Product_ID);
+            command.Parameters.AddWithValue("@Quantity", model.Quantity);
+            command.Parameters.AddWithValue("@Product_Size", model.Product_Size);
+            
+            connection.Open();
+            command.ExecuteNonQuery();
+            connection.Close();
+        }
+
+        public void Update(Product_Size_Quantity model)
+        {
+            SqlConnection connection = new SqlConnection(
+                "Server=140.126.146.49,7988;Database=2018Build;User Id=Build;Password = 123456789;"
+            );
+            var sql = "UPDATE Product_Size_Quantity SET Quantity=@Quantity WHERE Product_ID = @Product_ID and Product_Size=@Product_Size";
+            SqlCommand command = new SqlCommand(sql, connection);
+
+            command.Parameters.AddWithValue("@Product_ID", model.Product_ID);
+            command.Parameters.AddWithValue("@Quantity", model.Quantity);
+            command.Parameters.AddWithValue("@Product_Size", model.Product_Size);
+
+            connection.Open();
+            command.ExecuteNonQuery();
+            connection.Close();
+        }
+
+        public void Delete(Product_Size_Quantity model)
+        {
+            SqlConnection connection = new SqlConnection(
+                "Server=140.126.146.49,7988;Database=2018Build;User Id=Build;Password = 123456789;"
+            );
+            var sql = "DELETE FROM Product_Size_Quantity WHERE Product_ID = @Product_ID and Product_Size=@Product_Size";
+            SqlCommand command = new SqlCommand(sql, connection);
+            command.Parameters.AddWithValue("@Product_ID", model.Product_ID);
+             command.Parameters.AddWithValue("@Product_Size", model.Product_Size);
+
+            connection.Open();
+            command.ExecuteNonQuery();
+            connection.Close();
+        }
+
+
+        public Product_Size_Quantity GetByID(int Product_ID, string Product_Size)
+        {
+            SqlConnection connection = new SqlConnection(
+                "Server=140.126.146.49,7988;Database=2018Build;User Id=Build;Password = 123456789;"
+            );
+            var sql = "SELECT * FROM Product_Size_Quantity  WHERE Product_ID = @Product_ID and Product_Size=@Product_Size";
+            SqlCommand command = new SqlCommand(sql, connection);
+            command.Parameters.AddWithValue("@Product_ID", Product_ID);
+            command.Parameters.AddWithValue("@Product_Size", Product_Size);
+
+
+
+
+            connection.Open();
+            var reader = command.ExecuteReader(CommandBehavior.CloseConnection);
+            var productSizeQuantity = new Product_Size_Quantity();
+            while (reader.Read())
+            {
+                productSizeQuantity.Product_ID = Convert.ToInt32(reader.GetValue(reader.GetOrdinal("Product_ID")).ToString());
+                productSizeQuantity.Quantity = Convert.ToInt32(reader.GetValue(reader.GetOrdinal("Quantity")).ToString());
+                productSizeQuantity.Product_Size = reader.GetValue(reader.GetOrdinal("Product_Size")).ToString();
+
+
+            }
+            reader.Close();
+            return productSizeQuantity;
+        }
+
+        public IEnumerable<Product_Size_Quantity> GetAll()
+        {
+            SqlConnection connection = new SqlConnection(
+                "Server=140.126.146.49,7988;Database=2018Build;User Id=Build;Password = 123456789;"
+            );
+            var sql = "SELECT * FROM Product_Size_Quantity";
+            SqlCommand command = new SqlCommand(sql, connection);
+            connection.Open();
+            var reader = command.ExecuteReader(CommandBehavior.CloseConnection);
+            var productSizeQuantitys = new List<Product_Size_Quantity>();
+            while (reader.Read())
+            {
+                var productSizeQuantity = new Product_Size_Quantity();
+
+                productSizeQuantity.Product_ID = Convert.ToInt32(reader.GetValue(reader.GetOrdinal("Product_ID")).ToString());
+                productSizeQuantity.Quantity = Convert.ToInt32(reader.GetValue(reader.GetOrdinal("Quantity")).ToString());
+                productSizeQuantity.Product_Size = reader.GetValue(reader.GetOrdinal("Product_Size")).ToString();
+
+                productSizeQuantitys.Add(productSizeQuantity);
+            }
+            reader.Close();
+            return productSizeQuantitys;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
