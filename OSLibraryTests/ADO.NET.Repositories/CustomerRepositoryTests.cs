0e376cfa9f2f3da5cce3e57965a288c467369d22
OsbornOuO <osbornoops@gmail.com>
2018-05-09 21:34:26 +0800

    新增CustomerRepository GetAll Unit test

---
+++ OSLibraryTests/ADO.NET.Repositories/CustomerRepositoryTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OSLibrary.ADO.NET.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace OSLibrary.ADO.NET.Repositories.Tests
+{
+    [TestClass()]
+    public class CustomerRepositoryTests
+    {
+        [TestMethod()]
+        public void GetAllTest()
+        {
+            CustomerRepository repository = new CustomerRepository();
+            var result = repository.GetAll();
+            Assert.IsTrue(result.Count()==1);
+        }
+    }
+}
