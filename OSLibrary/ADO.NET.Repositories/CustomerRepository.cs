97b5e18e0fe53a7f397ff9789be4da9a113553f1
OsbornOuO <osbornoops@gmail.com>
2018-05-09 21:03:26 +0800

    Update Customer Repository

--- OSLibrary/ADO.NET.Repositories/CustomerRepository.cs
+++ OSLibrary/ADO.NET.Repositories/CustomerRepository.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -12,10 +13,99 @@ namespace OSLibrary.ADO.NET.Repositories
         public void Create(Customers model)
         {
             SqlConnection connection = new SqlConnection(
-            "data source=140.126.146.49,7988;initial catalog=2018Build;user id=Build;password=123456789;integrated security=true"
+                "Server=140.126.146.49,7988;Database=2018Build;User Id=Build;Password = 123456789;"
             );
-            var sql = "INSERT INTO Customers VALUES (@id, @name, @cname, @ctitle, @address, @city, @region, @postalcode, @country, @phone, @fax)";
-        }
+            var sql = "INSERT INTO Customers VALUES (@Account, @Name, @Password, @Email, @Phone, @Address)";
+            SqlCommand command = new SqlCommand(sql,connection);
+            command.Parameters.AddWithValue("@Account", model.Account);
+            command.Parameters.AddWithValue("@Name", model.Account);
+            command.Parameters.AddWithValue("@Password", model.Account);
+            command.Parameters.AddWithValue("@Email", model.Account);
+            command.Parameters.AddWithValue("@Phone", model.Account);
+            command.Parameters.AddWithValue("@Address", model.Account);
 
+            connection.Open();
+            command.ExecuteNonQuery();
+            connection.Close();
+        }
+        public void Update(Customers model)
+        {
+            SqlConnection connection = new SqlConnection(
+                "Server=140.126.146.49,7988;Database=2018Build;User Id=Build;Password = 123456789;"
+            );
+            var sql = "UPDATE Customers SET Acount = @Account, Name = @Name ,Password =  @Password,Email = @Email,Phone = @Phone,Address = @Address WHERE Account = @Account";
+            SqlCommand command = new SqlCommand(sql, connection);
+            command.Parameters.AddWithValue("@Account", model.Account);
+            command.Parameters.AddWithValue("@Name", model.Account);
+            command.Parameters.AddWithValue("@Password", model.Account);
+            command.Parameters.AddWithValue("@Email", model.Account);
+            command.Parameters.AddWithValue("@Phone", model.Account);
+            command.Parameters.AddWithValue("@Address", model.Account);
+
+            connection.Open();
+            command.ExecuteNonQuery();
+            connection.Close();
+        }
+        public void Delete(Customers model)
+        {
+            SqlConnection connection = new SqlConnection(
+                "Server=140.126.146.49,7988;Database=2018Build;User Id=Build;Password = 123456789;"
+            );
+            var sql = "DELETE FROM Customers WHERE Account = @Account";
+            SqlCommand command = new SqlCommand(sql, connection);
+            command.Parameters.AddWithValue("@Account", model.Account);
+
+            connection.Open();
+            command.ExecuteNonQuery();
+            connection.Close();
+        }
+        public Customers GetByID(string Account)
+        {
+            SqlConnection connection = new SqlConnection(
+                "Server=140.126.146.49,7988;Database=2018Build;User Id=Build;Password = 123456789;"
+            );
+            var sql = "SELECT * FROM Customers WHERE Account = @Account";
+            SqlCommand command = new SqlCommand(sql, connection);
+            command.Parameters.AddWithValue("@Account", Account);
+
+            connection.Open();
+            var reader = command.ExecuteReader(CommandBehavior.CloseConnection);
+            var customer = new Customers();
+            while(reader.Read())
+            {
+                customer.Account = reader.GetValue(reader.GetOrdinal("Account")).ToString();
+                customer.Name = reader.GetValue(reader.GetOrdinal("Name")).ToString();
+                customer.Password = reader.GetValue(reader.GetOrdinal("Password")).ToString();
+                customer.Email = reader.GetValue(reader.GetOrdinal("Email")).ToString();
+                customer.Phone = reader.GetValue(reader.GetOrdinal("Phone")).ToString();
+                customer.Address = reader.GetValue(reader.GetOrdinal("Address")).ToString();
+            }
+            reader.Close();
+            return customer;
+        }
+        public IEnumerable<Customers> GetAll()
+        {
+            SqlConnection connection = new SqlConnection(
+                "Server=140.126.146.49,7988;Database=2018Build;User Id=Build;Password = 123456789;"
+            );
+            var sql = "SELECT * FROM Customers";
+            SqlCommand command = new SqlCommand(sql, connection);
+            connection.Open();
+            var reader = command.ExecuteReader(CommandBehavior.CloseConnection);
+            var customers = new List<Customers>();
+            while(reader.Read())
+            {
+                var customer = new Customers();
+                customer.Account = reader.GetValue(reader.GetOrdinal("Account")).ToString();
+                customer.Name = reader.GetValue(reader.GetOrdinal("Name")).ToString();
+                customer.Password = reader.GetValue(reader.GetOrdinal("Password")).ToString();
+                customer.Email = reader.GetValue(reader.GetOrdinal("Email")).ToString();
+                customer.Phone = reader.GetValue(reader.GetOrdinal("Phone")).ToString();
+                customer.Address = reader.GetValue(reader.GetOrdinal("Address")).ToString();
+                customers.Add(customer);
+            }
+            reader.Close();
+            return customers;
+        }
     }
 }
