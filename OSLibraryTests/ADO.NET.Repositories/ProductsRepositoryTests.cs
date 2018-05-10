152444f5a2f18cd2941a85bda5e93ffbec17a045
Yi An Lin <lois.kiro@gmail.com>
2018-05-10 20:33:25 +0800

    新增DELETE() UPDATE() CREAT() 的tests

--- OSLibraryTests/ADO.NET.Repositories/ProductsRepositoryTests.cs
+++ OSLibraryTests/ADO.NET.Repositories/ProductsRepositoryTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OSLibrary;
 using OSLibrary.ADO.NET.Repositories;
 
 namespace OSLibraryTests.ADO.NET.Repositories
@@ -21,6 +22,50 @@ namespace OSLibraryTests.ADO.NET.Repositories
             ProductsRepository repository = new ProductsRepository();
             var result = repository.GetByProduct_ID(2);
             Assert.IsTrue(result.Product_Name=="短褲");
+        }
+        [TestMethod]
+        public void ProductsRepositoryTests_Create()
+        {
+            ProductsRepository repository = new ProductsRepository();
+            var model = new Products
+            {
+                Product_ID = 5,
+                Product_Name = "機能外套",
+                UnitPrice = 500,
+                Product_Types_Name = "外套",
+                Gender = "女"
+            };
+            repository.Create(model);
+            var result = repository.GetByProduct_ID(5); 
+            Assert.IsTrue(result!=null);
+        }
+
+        [TestMethod]
+        public void ProductsRepositoryTests_Update()
+        {
+              ProductsRepository repository = new ProductsRepository();
+            var model = new Products
+            {
+                Product_ID = 5,
+                Product_Name = "機能外套(限量)",
+                UnitPrice = 1500,
+                Product_Types_Name = "外套",
+                Gender = "女"
+            };
+            repository.Update(model);
+            var result = repository.GetByProduct_ID(5);
+            Assert.IsTrue(result.UnitPrice==1500);
+        }
+
+        [TestMethod]
+        public void ProductsRepositoryTests_DELETE()
+        {
+              ProductsRepository repository = new ProductsRepository();
+            var model = new Products
+            { Product_ID = 5};
+            repository.Delete(model);
+            var result = repository.GetByProduct_ID(5);
+            Assert.IsTrue(result == null);
         }
     }
 }
