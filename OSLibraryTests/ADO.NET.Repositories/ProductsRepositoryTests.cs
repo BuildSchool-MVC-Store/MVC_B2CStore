367c2e03fa82861fefa750a6ef167ea010307bd8
Yi An Lin <lois.kiro@gmail.com>
2018-05-10 21:32:33 +0800

    修改

--- OSLibraryTests/ADO.NET.Repositories/ProductsRepositoryTests.cs
+++ OSLibraryTests/ADO.NET.Repositories/ProductsRepositoryTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OSLibrary;
 using OSLibrary.ADO.NET.Repositories;
 
 namespace OSLibraryTests.ADO.NET.Repositories
@@ -13,14 +14,58 @@ namespace OSLibraryTests.ADO.NET.Repositories
         {
             ProductsRepository repository = new ProductsRepository();
             var result = repository.GetAll();
-            Assert.IsTrue(result.Count()>=2);
+            Assert.IsTrue(result.Count() >= 2);
         }
         [TestMethod]
         public void ProductsRepositoryTests_GetByProduct_ID()
         {
             ProductsRepository repository = new ProductsRepository();
-            var result = repository.GetByProduct_ID(1);
-            Assert.IsTrue(result.Product_Name=="黑色內褲");
+            var result = repository.GetByProduct_ID(2);
+            Assert.IsTrue(result.Product_Name == "短褲");
+        }
+        [TestMethod]
+        public void ProductsRepositoryTests_Create()
+        {
+            ProductsRepository repository = new ProductsRepository();
+            var model = new Products
+            {
+                Product_ID = 5,
+                Product_Name = "機能外套",
+                UnitPrice = 500,
+                Product_Types_Name = "外套",
+                Gender = "女"
+            };
+            repository.Create(model);
+            var result = repository.GetByProduct_ID(5);
+            Assert.IsTrue(result != null);
+        }
+
+        [TestMethod]
+        public void ProductsRepositoryTests_Update()
+        {
+            ProductsRepository repository = new ProductsRepository();
+            var model = new Products
+            {
+                Product_ID = 5,
+                Product_Name = "機能外套(限量)",
+                UnitPrice = 1500,
+                Product_Types_Name = "外套",
+                Gender = "女"
+            };
+            repository.Update(model);
+            var result = repository.GetByProduct_ID(5);
+            Assert.IsTrue(result.UnitPrice == 1500);
+        }
+
+        [TestMethod]
+        public void ProductsRepositoryTests_DELETE()
+        {
+            ProductsRepository repository = new ProductsRepository();
+            var model = new Products
+            { Product_ID = 5 };
+            repository.Delete(model);
+            var result = repository.GetByProduct_ID(5);
+            Assert.IsTrue(result == null);
         }
     }
 }
