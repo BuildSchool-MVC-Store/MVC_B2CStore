8460f72a2da06be84aaba47b0488853f32b3a542
User <User@DESKTOP-2LALK7K>
2018-05-10 15:10:15 +0800

    ADD ShoppingCartRepositoryTests

---
+++ OSLibraryTests/ADO.NET.Repositories/ShoppingCartRepositoryTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OSLibrary.ADO.NET.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace OSLibraryTests.ADO.NET.Repositories
+{
+    [TestClass]
+    public class ShoppingCartRepositoryTests
+    {
+        [TestMethod]
+        public void ShoppingCartRepositoryTests_GetAllTest()
+        {
+            ShoppingCartRepository repository = new ShoppingCartRepository();
+            var result = repository.GetAll();
+            Assert.IsTrue(result.Count() ==2);
+        }
+    }
+}
