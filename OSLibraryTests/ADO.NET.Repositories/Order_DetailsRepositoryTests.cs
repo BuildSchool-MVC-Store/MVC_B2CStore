e05709f95fd678bfc8203ae499ea428fc6d67ab0
丸子 小 <usj0326@gmail.com>
2018-05-10 15:06:36 +0800

    新增 Order_DetailsRepositoryTests

---
+++ OSLibraryTests/ADO.NET.Repositories/Order_DetailsRepositoryTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace OSLibraryTests.ADO.NET.Repositories
+{
+    [TestClass]
+    public class Order_DetailsRepositoryTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
