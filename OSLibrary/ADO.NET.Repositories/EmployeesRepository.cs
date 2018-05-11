af92823e5db125e622a240afd576ff5ccc706b02
丸子 小 <usj0326@gmail.com>
2018-05-11 21:10:34 +0800

    Update EmployessRepository

--- OSLibrary/ADO.NET.Repositories/EmployeesRepository.cs
+++ OSLibrary/ADO.NET.Repositories/EmployeesRepository.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using OSLibrary.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +10,12 @@ namespace OSLibrary.ADO.NET.Repositories
 {
     public class EmployeesRepository
     {
-        public void Create()
+        public void Create(Employees model)
         {
-
+            SqlConnection connection = new SqlConnection(
+                "Server=140.126.146.49,7988;Database=2018Build;User Id=Build;Password = 123456789;"
+                );
+            var sql = "INSERT INTO Employees VALUES (@Account, @Password, @Name, @Birthday, @Email, @Phone, @Address)";
         }
     }
 }
