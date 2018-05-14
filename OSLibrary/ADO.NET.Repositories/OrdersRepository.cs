5232b0d0e0efcaa7409ec55f1607da027b419528
alan tsai <zeroalan86@gmail.com>
2018-05-14 20:19:32 +0800

    Update Orders Dapper Create(),Update(),Delete(),GetByID()

--- OSLibrary/ADO.NET.Repositories/OrdersRepository.cs
+++ OSLibrary/ADO.NET.Repositories/OrdersRepository.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
+using Dapper;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,82 +12,58 @@ namespace OSLibrary.ADO.NET.Repositories
 {
     public class OrdersRepository
     {
+        private string strConnection = "Server=140.126.146.49,7988;Database=2018Build;User Id=Build;Password=123456789;";
         public void Create(Orders model)
         {
-            SqlConnection connection = new SqlConnection(
-                "Server=140.126.146.49,7988;Database=2018Build;User Id=Build;Password=123456789;"
-                );
-            var sql = "INSERT INTO Orders (Order_Date, Account, Pay, Transport, Order_Check, Total, TranMoney) VALUES (@Order_Date, @Account, @Pay, @Transport, @Order_Check, @Total, @TranMoney)";
-            SqlCommand command = new SqlCommand(sql, connection);
-            command.Parameters.AddWithValue("Order_Date", model.Order_Date);
-            command.Parameters.AddWithValue("Account", model.Account);
-            command.Parameters.AddWithValue("Pay", model.Pay);
-            command.Parameters.AddWithValue("Transport", model.Transport);
-            command.Parameters.AddWithValue("Order_Check", model.Order_Check);
-            command.Parameters.AddWithValue("Total", model.Total);
-            command.Parameters.AddWithValue("TranMoney", model.TranMoney);
-
-            connection.Open();
-            command.ExecuteNonQuery();
-            connection.Close();
+            using (SqlConnection Connection = new SqlConnection(strConnection))
+            {
+                var sql = "INSERT INTO Orders (Order_Date, Account, Pay, Transport, Order_Check, Total, TranMoney) VALUES (@Order_Date, @Account, @Pay, @Transport, @Order_Check, @Total, @TranMoney)";
+                var exec = Connection.Execute(sql, model);
+            }
         }
 
         public void Update(Orders model)
         {
-            SqlConnection connection = new SqlConnection(
-                "Server=140.126.146.49,7988;Database=2018Build;User Id=Build;Password=123456789;"
-                );
-            var sql = "SET IDENTITY_INSERT Orders ON UPDATE Orders SET Order_Date = @Order_Date, Account = @Account, Pay = @Pay, Transport = @Transport, Order_Check = @Order_Check, Total = @Total, TranMoney = @TranMoney WHERE Order_ID = @Order_ID SET IDENTITY_INSERT Orders OFF";
-            SqlCommand command = new SqlCommand(sql, connection);
-            command.Parameters.AddWithValue("Order_ID", model.Order_ID);
-            command.Parameters.AddWithValue("Order_Date", model.Order_Date);
-            command.Parameters.AddWithValue("Account", model.Account);
-            command.Parameters.AddWithValue("Pay", model.Pay);
-            command.Parameters.AddWithValue("Transport", model.Transport);
-            command.Parameters.AddWithValue("Order_Check", model.Order_Check);
-            command.Parameters.AddWithValue("Total", model.Total);
-            command.Parameters.AddWithValue("TranMoney", model.TranMoney);
-
-            connection.Open();
-            command.ExecuteNonQuery();
-            connection.Close();
+            using (SqlConnection Connection = new SqlConnection(strConnection))
+            {
+                var sql = "SET IDENTITY_INSERT Orders ON UPDATE Orders SET Order_Date = @Order_Date, Account = @Account, Pay = @Pay, Transport = @Transport, Order_Check = @Order_Check, Total = @Total, TranMoney = @TranMoney WHERE Order_ID = @Order_ID SET IDENTITY_INSERT Orders OFF";
+                var exec = Connection.Execute(sql, model);
+            }
         }
 
         public void Delete(Orders model)
         {
-            SqlConnection connection = new SqlConnection(
-                "Server=140.126.146.49,7988;Database=2018Build;User Id=Build;Password=123456789;"
-                );
-            var sql = "DELETE FROM Orders WHERE Order_ID = @Order_ID";
-            SqlCommand command = new SqlCommand(sql, connection);
-            command.Parameters.AddWithValue("@Order_ID", model.Order_ID);
-
-            connection.Open();
-            command.ExecuteNonQuery();
-            connection.Close();
+            using (SqlConnection Connection = new SqlConnection(strConnection))
+            {
+                var sql = "DELETE FROM Orders WHERE Order_ID = @Order_ID";
+                var exec = Connection.Execute(sql, model);
+            }
         }
 
         public Orders GetByOrder_ID(int Order_ID)
         {
-            SqlConnection connection = new SqlConnection(
-                "Server=140.126.146.49,7988;Database=2018Build;User Id=Build;Password = 123456789;"
-            );
-            var sql = "SELECT * FROM Orders WHERE Order_ID = @Order_ID";
-            SqlCommand command = new SqlCommand(sql, connection);
-            command.Parameters.AddWithValue("@Order_ID", Order_ID);
-
-            connection.Open();
-            var reader = command.ExecuteReader(CommandBehavior.CloseConnection);
-
-            var properties = typeof(Orders).GetProperties();
-
             Orders Order = null;
-            while(reader.Read())
+            using (SqlConnection Connection = new SqlConnection(strConnection))
             {
-                Order = DbReaderModelBinder<Orders>.Bind(reader);
+                var sql = "SELECT * FROM Orders WHERE Order_ID = @Order_ID";
+                Order = Connection.QueryFirst<Orders>(sql, new { Order_ID = Order_ID });
             }
-            reader.Close();
             return Order;
+            //SqlCommand command = new SqlCommand(sql, connection);
+            //command.Parameters.AddWithValue("@Order_ID", Order_ID);
+
+            //connection.Open();
+            //var reader = command.ExecuteReader(CommandBehavior.CloseConnection);
+
+            //var properties = typeof(Orders).GetProperties();
+
+            //Orders Order = null;
+            //while(reader.Read())
+            //{
+            //    Order = DbReaderModelBinder<Orders>.Bind(reader);
+            //}
+            //reader.Close();
+            //return Order;
         }
 
         public IEnumerable<Orders> GetAll()
