5278432e5103d917b0e5a19a8989ae23d97d6833
alan tsai <zeroalan86@gmail.com>
2018-05-18 12:30:06 +0800

    修正

--- OSLibraryTests/ADO.NET.Repositories/OrdersRepositoryTests.cs
+++ OSLibraryTests/ADO.NET.Repositories/OrdersRepositoryTests.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OSLibrary;
 using OSLibrary.ADO.NET.Repositories;
+using OSLibrary.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
