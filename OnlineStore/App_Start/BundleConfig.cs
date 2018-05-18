3301a63c0a456b74001ed59ce5c55c81210a9fe6
雅郁 黃 <huang22333@gmail.com>
2018-05-18 14:18:35 +0800

    不知道在幹嘛

--- OnlineStore/App_Start/BundleConfig.cs
+++ OnlineStore/App_Start/BundleConfig.cs
@@ -25,7 +25,9 @@ namespace OnlineStore
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/main.css",
+                      "~/Content/Modals.css"
+                      ));
         }
     }
 }
