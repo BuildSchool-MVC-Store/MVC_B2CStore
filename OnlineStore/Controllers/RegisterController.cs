364a87bbea8233a7f8a1f8002406f54ab727a4f3
OsbornOuO <osbornoops@gmail.com>
2018-05-25 11:45:57 +0800

    Revert "修改CSS"
    This reverts commit b6513b26e27e39f40ba068cf8cf4353ddc2ca28c.

---
+++ OnlineStore/Controllers/RegisterController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineStore.Controllers
+{
+    [RoutePrefix("Register")]
+    public class RegisterController : Controller
+    {
+        // GET: Register
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public ActionResult Register()
+        {
+            return View();
+        }
+    }
+}
