adb4846ebeb1f3c242fba6dbfd68b16c56db070c
OsbornOuO <osbornoops@gmail.com>
2018-05-28 21:28:21 +0800

    抽取 cookie驗證

--- OnlineStore/Controllers/CookieCheck.cs
+++ OnlineStore/Controllers/CookieCheck.cs
@@ -1,15 +1,34 @@
-﻿using System;
+﻿using OnlineStore.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
 
 namespace OnlineStore.Controllers
 {
     public class CookieCheck
     {
-        static public int check()
+        static public CookieModel check(HttpCookie cookie)
         {
-
+            var result = new CookieModel();
+            if (cookie == null)
+            {
+                result.check = false;
+                return result;
+            }
+            var ticket = FormsAuthentication.Decrypt(cookie.Value);
+            if (ticket.UserData == "12345")
+            {
+                result.IsAuthenticated = true;
+                result.Username = ticket.Name;
+            }
+            else
+            {
+                result.IsAuthenticated = false;
+            }
+            return result;
         }
     }
 }
---
+++ OnlineStore/Models/CookieModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineStore.Models
+{
+    public class CookieModel
+    {
+        public bool check { get; set; }
+        public bool IsAuthenticated { get; set; }
+        public string Username { get; set; }
+    }
+}
