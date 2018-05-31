4b7b81746a56e96efb2acb27fead26ba3ad7a49b
alan tsai <zeroalan86@gmail.com>
2018-05-31 18:24:49 +0800

    新增BackStageEmployeeController

---
+++ BackStageOnlineStore/Controllers/BackStageEmployeeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BackStageOnlineStore.Controllers
+{
+    public class BackStageEmployeeController : Controller
+    {
+        // GET: BackStageEmployee
+        public ActionResult SelectEmployee()
+        {
+            return View();
+        }
+
+        public ActionResult CreateEmployee()
+        {
+            return View();
+        }
+
+        public ActionResult UpdateEmployee()
+        {
+            return View();
+        }
+    }
+}
