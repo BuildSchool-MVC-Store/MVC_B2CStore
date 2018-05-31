1d5fb7c443054cdfc1ebd5d529d244338785a0c0
alan tsai <zeroalan86@gmail.com>
2018-05-31 21:23:08 +0800

    新增BackStageOrderController

---
+++ BackStageOnlineStore/Controllers/BackStageOrderController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BackStageOnlineStore.Controllers
+{
+    public class BackStageOrderController : Controller
+    {
+        // GET: BackStageOrder
+        public ActionResult SelectOrder()
+        {
+            return View();
+        }
+
+        public ActionResult CreateOrder()
+        {
+            return View();
+        }
+
+        //下面OrderDetails
+
+        public ActionResult SelectOrderDetails()
+        {
+            return View();
+        }
+
+        public ActionResult CreateOrderDetails()
+        {
+            return View();
+        }
+
+        public ActionResult UpdateOrderDetails()
+        {
+            return View();
+        }
+    }
+}
