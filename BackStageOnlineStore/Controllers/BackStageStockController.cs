29797a3e5e79f3b1c59da9f3e092c66ddd77ee92
alan tsai <zeroalan86@gmail.com>
2018-05-31 21:44:24 +0800

    新增BackStageStockController.cs

---
+++ BackStageOnlineStore/Controllers/BackStageStockController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BackStageOnlineStore.Controllers
+{
+    public class BackStageStockController : Controller
+    {
+        // GET: BackStageStock
+        public ActionResult SelectStock()
+        {
+            return View();
+        }
+
+        public ActionResult UpdateStock()
+        {
+            return View();
+        }
+    }
+}
