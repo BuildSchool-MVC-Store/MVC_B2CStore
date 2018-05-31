c4ac409fd318b9b5ade5c4316cafd36468bedff2
alan tsai <zeroalan86@gmail.com>
2018-06-01 01:48:02 +0800

    新增EmployeeService GetAll

---
+++ OSLibrary/ViewModels/EmployeeModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OSLibrary.ViewModels
+{
+    public class EmployeeModel
+    {
+        public string Account { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+        public DateTime Birthday { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+    }
+}
