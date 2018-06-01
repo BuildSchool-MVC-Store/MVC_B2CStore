b514a4813937c049d825a37e16e7772cd34529e3
Winni Huang <usj0326@gmail.com>
2018-06-01 10:49:05 +0800

    新增OrderDetailsModel

--- OSLibrary/Sevices/Order_DetailsService.cs
+++ OSLibrary/Sevices/Order_DetailsService.cs
@@ -1,5 +1,6 @@
 ﻿using OSLibrary.ADO.NET.Repositories;
 using OSLibrary.Models;
+using OSLibrary.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,13 +16,13 @@ namespace OSLibrary.Sevices
             return new Order_DetailsRepository().GetByOrderID(OrderID);
         }
 
-        public IEnumerable<Order_Details> BackStageGetAllOrderDetails()
+        public IEnumerable<Order_DetailsModel> BackStageGetAllOrderDetails()
         {
             Order_DetailsRepository detailsRepository = new Order_DetailsRepository();
-            var details = new List<Order_Details>();
+            var details = new List<Order_DetailsModel>();
             foreach(var item in detailsRepository.GetAll())
             {
-                var detail = new Order_Details
+                var detail = new Order_DetailsModel
                 {
                     Order_Details_ID = item.Order_Details_ID,
                     Order_ID = item.Order_ID,
