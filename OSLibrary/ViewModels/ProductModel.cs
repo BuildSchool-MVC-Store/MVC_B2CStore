f44332723ab2de83f7559b235a72b22acb4ae566
alan tsai <zeroalan86@gmail.com>
2018-06-01 17:30:06 +0800

    新增update product功能

--- OSLibrary/ViewModels/ProductModel.cs
+++ OSLibrary/ViewModels/ProductModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,10 +10,19 @@ namespace OSLibrary.ViewModels
     public class ProductModel
     {
         public int ProductID { get; set; }
+
+        [DisplayName("商品名稱")]
+        [Required(ErrorMessage = "請輸入名稱")]
         public string Name { get; set; }
+
+        [DisplayName("價格")]
+        [Required(ErrorMessage = "請輸入金額")]
         public decimal Price { get; set; }
+
         public string CategoryName { get; set; }
+
         public string Gender { get; set; }
+
         public string ImageUrl { get; set; }
     }
 }
