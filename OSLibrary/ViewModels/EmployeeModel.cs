e00dfad188dee10cf46de315404dce7dde63b94b
alan tsai <zeroalan86@gmail.com>
2018-06-01 16:52:38 +0800

    UPdate layout

--- OSLibrary/ViewModels/EmployeeModel.cs
+++ OSLibrary/ViewModels/EmployeeModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,11 +11,17 @@ namespace OSLibrary.ViewModels
     public class EmployeeModel
     {
         public string Account { get; set; }
+        
         public string Password { get; set; }
+        
         public string Name { get; set; }
+        
         public DateTime Birthday { get; set; }
+
         public string Phone { get; set; }
+
         public string Email { get; set; }
+
         public string Address { get; set; }
     }
 }
