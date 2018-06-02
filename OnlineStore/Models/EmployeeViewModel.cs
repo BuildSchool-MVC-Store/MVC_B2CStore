6bed28c0634fb38ee6db178cc526c1fd3a29623f
OsbornOuO <osbornoops@gmail.com>
2018-06-02 13:37:17 +0800

    測試 驗證屬性

---
+++ OnlineStore/Models/EmployeeViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace OnlineStore.Models
+{
+    public class EmployeeViewModel
+    {
+        [Required(ErrorMessage ="請輸入帳號")]
+        [StringLength(50)]
+        public string Account { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Password { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [Column(TypeName = "date")]
+        public DateTime Birthday { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Phone { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Address { get; set; }
+    }
+}
