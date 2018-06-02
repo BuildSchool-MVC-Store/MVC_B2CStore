1cf29c2c934adc6960b1cdd4a747e1cd74cf0062
alan tsai <zeroalan86@gmail.com>
2018-06-02 22:19:12 +0800

    UpdateEmployee屬性類別

--- OSLibrary/ViewModels/EmployeeModel.cs
+++ OSLibrary/ViewModels/EmployeeModel.cs
@@ -10,20 +10,32 @@ namespace OSLibrary.ViewModels
 {
     public class EmployeeModel
     {
+        [DisplayName("帳號")]
+        [Required]
         public string Account { get; set; }
         
         [DisplayName("密碼")]
         [Required(ErrorMessage = "請輸入密碼")]
         public string Password { get; set; }
-        
+
+        [DisplayName("姓名")]
+        [Required]
         public string Name { get; set; }
-        
+
+        [DisplayName("生日")]
+        [Required]
         public DateTime Birthday { get; set; }
 
+        [DisplayName("手機")]
+        [Required]
         public string Phone { get; set; }
 
+        [DisplayName("信箱")]
+        [Required]
         public string Email { get; set; }
 
+        [DisplayName("地址")]
+        [Required]
         public string Address { get; set; }
     }
 }
