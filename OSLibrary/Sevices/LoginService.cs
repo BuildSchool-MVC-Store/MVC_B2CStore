b1cbfe4b2f466f0e92db90ce1bf3f994a87ab10a
alan tsai <zeroalan86@gmail.com>
2018-06-06 11:42:50 +0800

    新增LoginService

---
+++ OSLibrary/Sevices/LoginService.cs
@@ -0,0 +1,43 @@
+﻿using OSLibrary.ADO.NET.Repositories;
+using OSLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OSLibrary.Sevices
+{
+    public class LoginService
+    {
+        public string LoginCheck(string account, string password)
+        {
+            var Employee = new EmployeesRepository();
+            var check = Employee.GetByEmployeesAccount(account);
+            //判斷有無此會員
+            if(check != null)
+            {
+                //驗證信if(String.IsNullOrWhiteSpace(check.AuthCode))
+                //帳密確認
+                if(PasswordCheck(check, password))
+                {
+                    return "";
+                }
+                else
+                {
+                    return "密碼錯誤";
+                }
+            }
+            else
+            {
+                return "查無此帳號，請去申請";
+            }
+        }
+
+        public bool PasswordCheck(Employees checkemployee, string password)
+        {
+            bool result = checkemployee.Password.Equals(password);//hashpassword(password)??
+            return result;
+        }
+    }
+}
---
+++ OSLibrary/ViewModels/LoginViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OSLibrary.ViewModels
+{
+    public class LoginViewModel
+    {
+        [DisplayName("帳號")]
+        [Required(ErrorMessage = "請輸入會員帳號")]
+        public string Account { get; set; }
+
+        [DisplayName("密碼")]
+        [Required(ErrorMessage = "請輸入密碼")]
+        public string Password { get; set; }
+    }
+}
