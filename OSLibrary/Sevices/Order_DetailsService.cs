7808ac7271d20359d4f7cfdb2a7e53d970b3028a
OsbornOuO <osbornoops@gmail.com>
2018-06-08 02:29:39 +0800

    新增 找OrderDetailID的方法

---
+++ OSLibrary/Sevices/Order_DetailsService.cs
@@ -0,0 +1,19 @@
+﻿using OSLibrary.ADO.NET.Repositories;
+using OSLibrary.Containers;
+using OSLibrary.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OSLibrary.Sevices
+{
+    public class Order_DetailsService
+    {
+        public Person_OrderDetail GetByOrderDetail_ID(int OrderDetail_ID)
+        {
+            return RepositoryContainer.GetInstance<Order_DetailsRepository>().GetByOrderDetail_IDOfView(OrderDetail_ID);
+        }
+    }
+}
