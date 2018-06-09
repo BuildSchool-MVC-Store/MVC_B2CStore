481539fcbd9d145e181eb0796cb7e099e1d78d47
OsbornOuO <osbornoops@gmail.com>
2018-06-10 02:45:45 +0800

    新增 分類的Service

---
+++ OSLibrary/Sevices/CategoryService.cs
@@ -0,0 +1,19 @@
+﻿using OSLibrary.ADO.NET.Repositories;
+using OSLibrary.Containers;
+using OSLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OSLibrary.Sevices
+{
+    public class CategoryService
+    {
+        public IEnumerable<Category> GetCategory()
+        {
+            return RepositoryContainer.GetInstance<CategoryRepository>().GetAll();
+        }
+    }
+}
